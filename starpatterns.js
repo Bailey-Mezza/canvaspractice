6814eb9478e77b9ccefe19958a3319682f98b5bd
Bailey Mezza <bmezza2812@gmail.com>
2025-05-05 22:31:35 +0100

    started working on Star patterns to create a cool rotating night sky effect.

---
+++ starpatterns.js
@@ -0,0 +1,82 @@
+var canvas = document.querySelector('canvas');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+var content = canvas.getContext('2d');
+
+//variables
+var mouse = {
+    x: innerWidth / 2,
+    y: innerHeight / 2
+}
+
+var colorArray = [
+    '#A8DADC',
+    '#457B9D',
+    '#BEE9E8',
+    '#1D3557'
+]
+
+//event listeners
+addEventListener('mousemove',
+    event => {
+        mouse.x = event.x;
+        mouse.y = event.y;
+    })
+
+addEventListener('resize',
+    () => {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+    })
+
+//Utility Functions
+function randomIntFromRange(min, max){
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+//Objects
+class Object {
+    constructor(x, y, radius) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = colorArray[Math.floor(Math.random() * colorArray.length)];
+    }
+
+    draw() {
+        content.beginPath();
+        content.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        content.fillStyle = this.color;
+        content.fill();
+        content.closePath();
+    }
+
+    update() {
+        this.draw();
+    }
+}
+
+let objects;
+//Implementation
+function init() {
+    objects = [];
+
+    for (let index = 0; index < 10; index++) {
+        //objects.push(new Object())
+    }
+}
+
+//Animate loop
+function animate() {
+    requestAnimationFrame(animate);
+    content.clearRect(0, 0, innerWidth, innerHeight);
+
+    // objects.forEach(object => {
+    //     object.update();
+    // });
+}
+
+init();
+animate();
