630218947bcc02699a300b0bcb089bc3ba54a43d
Bailey Mezza <bmezza2812@gmail.com>
2025-05-08 14:43:07 +0100

    Added player, projectiles that shoot toward mouse click and enemies that travel toward player.More will be added to detect collisions.

--- simplegame.js
+++ simplegame.js
@@ -12,19 +12,15 @@ var mouse = {
 }
 
 var colorArray = [
-    '#A8DADC',
-    '#457B9D',
-    '#BEE9E8',
-    '#1D3557'
+    '#FF6B6B',
+    '#E63946',
+    '#D90429',
+    '#EF476F',
+    '#F94144',
+    '#F37272',
 ]
 
 //event listeners
-addEventListener('mousemove',
-    event => {
-        mouse.x = event.x;
-        mouse.y = event.y;
-    })
-
 addEventListener('resize',
     () => {
         canvas.width = window.innerWidth;
@@ -32,17 +28,17 @@ addEventListener('resize',
     })
 
 //Utility Functions
-function randomIntFromRange(min, max){
+function randomIntFromRange(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 //Objects
-class Object {
+class Player {
     constructor(x, y, radius) {
         this.x = x;
         this.y = y;
         this.radius = radius;
-        this.color = colorArray[Math.floor(Math.random() * colorArray.length)];
+        this.color = 'rgb(255,255,255)';
     }
 
     draw() {
@@ -58,24 +54,104 @@ class Object {
     }
 }
 
-let objects;
-//Implementation
-function init() {
-    objects = [];
+class Projectile {
+    constructor(x, y, radius, velocity) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color ='rgb(149, 0, 255)';
+        this.velocity = { x: velocity.x, y: velocity.y };
+    }
 
-    for (let index = 0; index < 10; index++) {
-        //objects.push(new Object())
+    draw() {
+        content.beginPath();
+        content.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        content.fillStyle = this.color;
+        content.fill();
+        content.closePath();
+    }
+
+    update() {
+        this.x += this.velocity.x;
+        this.y += this.velocity.y;
+        this.draw();
     }
 }
+
+class Enemy {
+    constructor(x, y, radius, velocity) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = colorArray[Math.floor(Math.random() * colorArray.length)];
+        this.velocity = { x: velocity.x, y: velocity.y };
+    }
+
+    draw() {
+        content.beginPath();
+        content.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        content.fillStyle = this.color;
+        content.fill();
+        content.closePath();
+    }
+
+    update() {
+        this.x += this.velocity.x;
+        this.y += this.velocity.y;
+        this.draw();
+    }
+}
+
+let projectiles = [];
+let enemies;
+let player;
+//Implementation
+function init() {
+    enemies = [];
+
+    player = new Player(canvas.width / 2, canvas.height / 2, 20);
+
+    setInterval(() => {
+        const x = randomIntFromRange(0, canvas.width);
+        const y = randomIntFromRange(0, canvas.height);
+        const radius = randomIntFromRange(10, 50);
+        const angle = Math.atan2(y - canvas.height / 2, x - canvas.width / 2)
+        const velocity = {
+            x: -Math.cos(angle),
+            y: -Math.sin(angle)
+        }
+        enemies.push(new Enemy(x, y, radius, velocity));
+    }, 1000)
+}
+
+addEventListener('click',
+    event => {
+        mouse.x = event.x;
+        mouse.y = event.y;
+        const angle = Math.atan2(mouse.y - canvas.height / 2, mouse.x - canvas.width / 2)
+        const velocity = {
+            x: Math.cos(angle) * 5,
+            y: Math.sin(angle) * 5
+        }
+        projectiles.push(new Projectile(canvas.width / 2, canvas.height / 2, 5, velocity));
+
+    })
 
 //Animate loop
 function animate() {
     requestAnimationFrame(animate);
-    content.clearRect(0, 0, innerWidth, innerHeight);
+    content.fillStyle = 'rgba(0,0,0, 0.5)'
+    content.fillRect(0, 0, innerWidth, innerHeight);
 
-    // objects.forEach(object => {
-    //     object.update();
-    // });
+    player.update();
+
+    projectiles.forEach(projectile => {
+        projectile.update();
+    });
+
+    enemies.forEach(enemy => {
+        enemy.update();
+    });
 }
 
 init();
