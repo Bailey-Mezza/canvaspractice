2396ba31a3874fc4ba43a86c6e6aa005841c6bc9
Bailey Mezza <bmezza2812@gmail.com>
2025-05-25 11:12:43 +0100

    Finished zoom feature

--- zoom.js
+++ zoom.js
@@ -1,98 +1,155 @@
 var canvas = document.querySelector('canvas');
- 
- canvas.width = window.innerWidth;
- canvas.height = window.innerHeight;
- 
- var content = canvas.getContext('2d');
- 
- //variables
- var mouse = {
-     x: 10,
-     y: 10
- }
- 
- var colorArray = [
-     '#A8DADC',
-     '#457B9D',
-     '#BEE9E8',
-     '#1D3557'
- ]
- 
- //event listeners
- window.addEventListener('mousemove', 
-     function (event) {
-         mouse.x = event.x;
-         mouse.y = event.y;
- })
- 
- window.addEventListener('resize', 
-     function () {
-         canvas.width = window.innerWidth;
-         canvas.height = window.innerHeight;
- })
- 
- //Utility Functions
- function randomIntFromRange(min,max) {
-     return Math.floor[Math.random() * (max-min+1) + min]
- }
 
- function checkCollision(x1, x2, y1, y2, width1, width2, height1, height2) {
-    if (
-        x1 < x2 + width2 &&
-        x1 + width1 > x2 &&
-        y1 < y2 + height2 &&
-        y1 + height1 > y2
-     ){
-        return true;
-     } else {
-        return false;
-     }
- }
- 
- //Objects
- function Square(x, y, height, width, color) {
-     this.x = x;
-     this.y = y;
-     this.height = height;
-     this.width = width;
-     this.color = color;
- 
-     this.draw = function () {
-         content.fillStyle = this.color;
-         content.fillRect(this.x, this.y, this.height, this.width);
-     }
- 
-     this.update = function () {
-         this.draw();
-     }
- }
- 
- //Implementation
- let square1;
- let square2;
- function init() {
-     square1 = new Square(300, 300, 200, 200, 'black');
-     square2 = new Square(mouse.x, mouse.y, 50, 50, 'red');
- }
- 
- //Animate loop
- function animate() {
-     requestAnimationFrame(animate);
-     content.clearRect(0, 0, innerWidth, innerHeight);
-     square1.update();
-     square2.x = mouse.x;
-     square2.y = mouse.y;
-     square2.update();
- 
-     let isColliding = checkCollision(square1.x, square2.x, square1.y, square2.y, 
-        square1.width, square2.width, square1.height, square2.height);
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
-     if(isColliding) {
-        square1.color = 'red';
-    } else {
-        square1.color = 'black';
+var content = canvas.getContext('2d');
+
+//variables
+var mouse = {
+    x: 10,
+    y: 10
+}
+
+var colorArray = [
+    '#A8DADC',
+    '#457B9D',
+    '#BEE9E8',
+    '#1D3557'
+]
+
+let scale = 1;
+let count = 0;
+
+//event listeners
+window.addEventListener('mousemove',
+    function (event) {
+        mouse.x = event.x;
+        mouse.y = event.y;
+    })
+
+window.addEventListener('resize',
+    function () {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+    })
+
+window.addEventListener('keydown', function (event) {
+    const panSpeed = 20 / scale; // speed adjusted to zoom level
+
+    switch (event.code) {
+        case 'ArrowUp':
+        case 'KeyW':
+            camera.y -= panSpeed;
+            break;
+        case 'ArrowDown':
+        case 'KeyS':
+            camera.y += panSpeed;
+            break;
+        case 'ArrowLeft':
+        case 'KeyA':
+            camera.x -= panSpeed;
+            break;
+        case 'ArrowRight':
+        case 'KeyD':
+            camera.x += panSpeed;
+            break;
+        case 'Digit1':
+            scale = Math.min(scale + 0.05, 2);
+            break;
+        case 'Digit2':
+            scale = Math.max(scale - 0.05, 0.1);
+            break;
+    }
+    console.log(scale);
+});
+
+
+
+//Utility Functions
+function randomIntFromRange(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function getDistance(x1, y1, x2, y2) {
+    let xDistance = x2 - x1;
+    let yDistance = y2 - y1;
+
+    return Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
+}
+
+//Objects
+let camera = {
+    x: 0,
+    y: 0
+};
+
+function Particle(x, y, radius) {
+    this.x = x;
+    this.y = y;
+    this.velocity = {
+        x: randomIntFromRange(-2.5, 2.5),
+        y: randomIntFromRange(-2.5, 2.5)
+    }
+    this.radius = radius;
+    this.color = colorArray[Math.floor(Math.random() * colorArray.length)];
+    this.mass = 1;
+    this.opacity = 0;
+
+    this.draw = () => {
+        content.beginPath();
+        content.fillStyle = this.color;
+        content.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        content.fill();
+        content.closePath();
+    }
+
+
+    this.update = () => {
+        this.draw();
+
+        this.x += this.velocity.x;
+        this.y += this.velocity.y;
+
+        if (this.x + this.radius > 5000 || this.x - this.radius < -5000) {
+            this.velocity.x = -this.velocity.x;
+        }
+        if (this.y + this.radius > 5000 || this.y - this.radius < -5000) {
+            this.velocity.y = -this.velocity.y;
+        }
+
+
     }
 }
- 
- init();
- animate();
+
+//Implementation
+let particles = [];
+function init() {
+    for (let i = 0; i < 200; i++) {
+        let radius = 50;
+        x = randomIntFromRange(radius, 5000);
+        y = randomIntFromRange(radius, 5000);
+    
+        particles.push(new Particle(x, y, radius))
+    }
+}
+
+//Animate loop
+function animate() {
+    requestAnimationFrame(animate);
+
+    content.setTransform(1, 0, 0, 1, 0, 0);
+    content.clearRect(0, 0, canvas.width, canvas.height);
+
+    content.translate(canvas.width / 2, canvas.height / 2);
+    content.scale(scale, scale);
+    content.translate(-camera.x, -camera.y);
+
+    particles.forEach(particle => {
+        particle.update();
+    });
+}
+
+
+init();
+animate();
